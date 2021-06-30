48362e3c50b12921a7946c95ce5e6e695ae2037d
Armela <armgjoka@gmail.com>
2021-06-30 12:46:54 +0200

    'reset'

--- src/calculator.class.ts
+++ src/calculator.class.ts
@@ -1,29 +1,9 @@
 export class Calculator {
 
-    constructor() {
-        console.log('Constructor');
-        this.convertToRomanNumber(5);
-    }
-
     arabicNumbers = [1, 5, 10, 50, 100, 500, 1000];
     romanNumbers = ['I', 'V', 'X', 'L', 'C', 'D', 'M']
 
     convertToRomanNumber(value : number) : string {
-        if (value < 1) {
-            throw new RangeError
-            ('Values less than 1 can not be converted to Roman numbers');
-        }
-
-        let roman = '';
-
-        for (let i = 6; i >= 0; i--) {
-            const arabicValue = this.arabicNumbers[i];
-            while (value >= arabicValue) {
-                roman += this.romanNumbers[i];
-                value -= arabicValue;
-            }
-        }
-
-        return roman;
+        return '';
     }
 }
--- src/calculator.test.ts
+++ src/calculator.test.ts
@@ -8,22 +8,7 @@ describe("Calculator", () => {
         calculator = new Calculator();
     });
 
-    it("should convert 1 to I", () => {
-        expect(calculator.convertToRomanNumber(1)).toBe('I');
+    it("should convert number to empty string", () => {
+        expect(calculator.convertToRomanNumber(1)).toBe('');
     });
-
-    it("should convert 2 to II", () => {
-        expect(calculator.convertToRomanNumber(2)).toBe('II');
-    });
-
-    it("should throw an error for numbers less than 0", () => {
-        try {
-            calculator.convertToRomanNumber(0);
-        } catch (error) {
-            expect(error).toBeInstanceOf(RangeError);
-            expect(error).toHaveProperty('message', 'Values less than 1 can not be converted to Roman numbers');
-        }
-    });
-
-
 });
